ec93268834263535bbdbefa791dd589aef14035d
K2302 <95227135+K2302@users.noreply.github.com>
2023-01-08 08:24:42 +0530

    Create priyansh.cpp

---
+++ My template/priyansh.cpp
@@ -0,0 +1,14 @@
+/*+++++++++++++++++++++++++++  FUNCTION +++++++++++++++++++++++++++++++++++++++++++++++*/
+ 
+int gcd(int a, int b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
+int lcm(int a, int b){return (a/gcd(a,b)*b);}
+int expo(int a, int b, int mod) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(int a, int b, int*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); int x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+int mminv(int a, int b) {int arr[3]; extendgcd(a, b, arr); return arr[0];}  //for non prime b
+int mminvprime(int a, int b) {return expo(a, b - 2, b);}
+int power(int a, int b){if(b==0){ return 1;}if(b%2==0){int t = power(a, b/2);return t*t;}else{int t = power(a, (b-1)/2);
+t = t*t;return a*t;}}
+int mod_add(int a, int b, int m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+int mod_mul(int a, int b, int m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+int mod_sub(int a, int b, int m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+int mod_div(int a, int b, int m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}

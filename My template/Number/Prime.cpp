85314ea768cb8aa9be47c99b079163bbbd99f18d
K2302 <95227135+K2302@users.noreply.github.com>
2023-01-21 08:47:39 +0530

    Update Prime.cpp

--- My template/Number/Prime.cpp
+++ My template/Number/Prime.cpp
@@ -1,26 +1,7 @@
-vector<int> primeFact(int n);
+
 vector<int> siv(1000001);
 
-vector<int> primeFact(int n)
-{
-   vector<int>v;
-     for (int i = 2; i * i <= n; i++)
-     {
-          if (n % i == 0)
-          {
-               while (n % i == 0)
-               {
-                    v.push_back(i);
-                    n = n / i;
-               }
-          }
-     }
-     if (n > 1)
-     {
-          v.push_back(n);
-     }
-     return v;
-}
+
 void sieve(int n)
 {
  

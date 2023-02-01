1a4c088e8d0ad73513d8b72e77bb33fde8ed5d6a
K2302 <95227135+K2302@users.noreply.github.com>
2023-02-01 17:58:51 +0530

    Update factors.cpp

--- My template/Number/factors.cpp
+++ My template/Number/factors.cpp
@@ -3,11 +3,10 @@ vector<long long> trial_division1(long long n) {
     for (long long d = 2; d * d <= n; d++) {
         while (n % d == 0) {
             factorization.push_back(d);
-            n /= d;
+            if(d!=(n/d))factorization.push_back(n / d);
         }
     }
     if (n > 1)
         factorization.push_back(n);
     return factorization;
 }
- 

24fa2cbe2c915e56be7231673cb57177d3f395af
K2302 <95227135+K2302@users.noreply.github.com>
2023-02-03 07:26:02 +0530

    Create deci_to_binary.cpp

---
+++ My template/Number/deci_to_binary.cpp
@@ -0,0 +1,13 @@
+string DecimalToBinary(int num)
+{
+    string str;
+      while(num){
+      if(num & 1) // 1
+        str+='1';
+      else // 0
+        str+='0';
+      num>>=1; // Right Shift by 1 
+    }   
+    reverse(all(str));
+      return str;
+}
